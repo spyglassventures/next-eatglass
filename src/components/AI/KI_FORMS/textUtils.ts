40ebba0c8e04975ab036a3a637d3c21852d7c165
Daniel Mueller <dm@spyglassventures.ch>
2024-12-13 08:42:55 +0100

    fix in utils

--- src/components/AI/KI_FORMS/textUtils.ts
+++ src/components/AI/KI_FORMS/textUtils.ts
@@ -1,15 +1,16 @@
-// utils/textUtils.ts
+// utils/textUtils.ts used in next-eatglass/src/components/AI/KI_FORMS/kiForms_chat_structure.tsx
+// there is similar code in gen_chat_structure.tsx
 
-// export const countTokens = (text: string) => {
-//     return text.split(/\s+/).filter(Boolean).length;
-// };
+export const countTokens = (text: string) => {
+    return text.split(/\s+/).filter(Boolean).length;
+};
 
-// export const getTokenStatus = (tokens: number) => {
-//     if (tokens > 5000) {
-//         return { color: 'text-red-600', message: 'Zu viele Wörter', icon: 'FaTimesCircle' };
-//     } else if (tokens > 3500) {
-//         return { color: 'text-orange-300', message: 'Eventuell zu viele Wörter', icon: 'FaExclamationTriangle' };
-//     } else {
-//         return { color: 'text-green-600', message: 'Eingabe gu 2t' };
-//     }
-// };
+export const getTokenStatus = (tokens: number) => {
+    if (tokens > 5000) {
+        return { color: 'text-red-600', message: 'Zu viele Wörter', icon: 'FaTimesCircle' };
+    } else if (tokens > 3500) {
+        return { color: 'text-orange-300', message: 'Eventuell zu viele Wörter', icon: 'FaExclamationTriangle' };
+    } else {
+        return { color: 'text-green-600', message: 'Eingabe gu 2t' };
+    }
+};
