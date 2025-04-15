0709f8be839da13247384abf0a3ff53a20ac41f4
Daniel Mueller <dm@spyglassventures.ch>
2025-04-15 14:37:50 +0200

    fixes

--- src/config/ai/ai_tabs/grippe_message.ts
+++ src/config/ai/ai_tabs/grippe_message.ts
@@ -5,20 +5,13 @@ export const warning_msg = 'Closed-Beta Test: Schlüsselinhalte anklicken ➜ Gr
 
 export const followupBtn = ['mehr Argumente', 'eloquenter formulieren', 'übersetze in Englisch'];
 
-// if in existence, then the cloud buttons will be displayed. Otherwise the request to enter the prompt will be displayed.
 export const inputCloudBtn = {
     Demografie: [
-        "0-1 Jahre, Säugling",
-        "1-3 Jahre, Kleinkind",
-        "4-10 Jahre alt",
-        "11-18 Jahre alt",
-        "19-50 Jahre alt",
-        "51-70 Jahre alt",
-        "über 70 Jahre",
-        "männlich",
-        "weiblich"
+        "männlich Pat.",
+        "weiblich Pat"
     ],
     Symptome: [
+        "Grippaler Infekt",
         "Husten",
         "Fieber",
         "Halsschmerzen",
@@ -29,6 +22,70 @@ export const inputCloudBtn = {
         "Abgeschlagenheit",
         "Nasennebenhöhlenentzündung"
     ],
+    Dauer: [
+        "seit 1 Tag",
+        "seit 2 Tage",
+        "seit wenigen Tagen",
+        "seit 3 Tage",
+        "seit 4 Tage",
+        "seit 5 Tage",
+        "seit 1 Woche",
+        "seit 2 Wochen",
+    ],
+
+    Untersuchung: [
+        "körperliche Untersuchung",
+        "Hinweis auf Sinusitis",
+        "Hinweis viralen Infekt",
+        "Hinweis auf bakterielle Infektion",
+        "Hinweis auf Allergie",
+        "Hinweis auf Bronchitis",
+        "Hinweis auf Pneumonie",
+    ],
+
+    Beschreibung: [
+        "Ohr Untersuchung",
+        "Rachen Untersuchung",
+        "Lunge Untersuchung",
+        "Nasen Untersuchung",
+        "unauffällig",
+        "blande/TFgeröter",
+        "oral gerötet, bis leicht rosig",
+        "Hinweis auf Nasennebenhöhlen",
+        "Beteiligung",
+        "Lunge frei",
+        "kein exnthem",
+        "oropharyngeal gerötet",
+        "leicht gerötetes Trommelfell",
+    ],
+
+    Labor: [
+        "Labor - leichter Leukozytose",
+        "Labor - CRP von: []",
+        "Labor - HBa1C von: [] ",
+        "CRP leicht erhöht",
+        "Temperatur: []°C",
+        "Sauerstoffsättigung: []%"
+    ],
+
+    Krankmeldung: [
+        "KM für 3 Tage",
+        "KM für 5 Tage",
+        "KM für 7 Tage"
+
+    ],
+
+    Wiedervorstellung: [
+        "WV bei Verschlechterung",
+        "WV bei Fieber > 39°C",
+        "WV bei Atemnot",
+        "WV bei Bewusstseinsveränderung",
+        "WV bei anhaltendem Husten",
+        "WV bei anhaltenden Halsschmerzen"
+    ],
+
+
+
     Wirkstoffe: [
         "Paracetamol",
         "Ibuprofen",
@@ -68,6 +125,16 @@ export const inputCloudBtn = {
         "Reisetätigkeit",
         "enge Familienkontakte mit Grippe",
         "beruflicher Kontakt zu vielen Menschen"
+    ],
+
+    Verlauf: [
+        "konservatives Vorgehen empfohlen",
+        "Analgesie",
+        "körperliche Schonung",
+        "ausreichend Flüssigkeit",
+        "schleimlösende Medikation",
+        "Krankmeldung für 3 Tage",
+        "Wiedervorstellung bei Verschlechterung"
     ]
 };
 
